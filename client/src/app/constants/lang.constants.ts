3567ba45ca88465ffae81a723b43a945005f6ffd
Nikolay Borzunov <forest3694@ya.ru>
2022-01-09 15:05:48 +0300

    [FE] Header and footer layout

---
+++ client/src/app/constants/lang.constants.ts
@@ -0,0 +1,13 @@
+export const lang = {
+  RU: {
+    labels: {
+      SEARCH: 'Поиск игр',
+      MARKET: 'Магазин',
+      EQUIPMENT: 'Инвентарь',
+      LOGIN: 'Войти',
+      FOOTER_COPYRIGHT: '2022 Monopoly. All Rights Reserved.'
+    }
+  }
+}
+
+export const currentLang = 'RU'
